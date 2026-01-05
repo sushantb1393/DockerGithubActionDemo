57706a3a24a6de0f01c1d75fd7ba542ef2879bcb
sushantb1393 <sushantb1393@gmail.com>
2026-01-05 18:42:49 +0530

    test the docker trigger of github action

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World Test! from GitHub Actions + Dockerr"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
